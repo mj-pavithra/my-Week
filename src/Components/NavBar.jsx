7a01107405cf9d62d9dcf16341cff5e35e112d24
mjpavithra <mjpavithra6520@gmail.com>
2024-02-18 18:07:43 +0530

    change nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React , {useEffect,useState} from "react";
 import LeftNav from "../Components/LeftNav";
 import MiddleNav from "./MiddleNav";
 import RightNav from "../Components/RightNav";
 
 import "../Styles/NavBar.css";
 function NavBar({segmentsList}) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  
     return (
             <div className="navbar">
                 <LeftNav className="nav-part"/>
                 <MiddleNav className="nav-part" segments={segmentsList}/>
-                <RightNav className="nav-part"/>
+                {windowWidth > 968 ? <RightNav className="nav-part"/> : null}
             </div>
     );
     }
